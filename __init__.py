68d218a51c24be9868fb652853a66ad8117bda60
Chris Choy <chrischoy@ai.stanford.edu>
2018-05-17 15:59:18 +0900

    import SparsePooling

--- __init__.py
+++ __init__.py
@@ -7,5 +7,6 @@ sys.path.append(file_dir)
 from SparseConvolution import SparseConvolutionFunction, SparseConvolution, \
     SparseConvolutionTransposeFunction, SparseConvolutionTranspose, \
     Metadata, RegionType
+from SparsePooling import SparseMaxPoolingFunction, SparseMaxPooling
 
 from SparseConvolutionNetwork import SparseConvolutionNetwork
