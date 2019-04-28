2ce3bb6fcda4c15b30643c84e7ce596ed2d6ce9b
Chris Choy <chrischoy@ai.stanford.edu>
2019-04-27 23:15:08 -0700

    auto versioning

--- MinkowskiEngine/__init__.py
+++ MinkowskiEngine/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.2.2"
+
 import os
 import sys
 
--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
+import codecs
 import os
+import re
 import time
 from subprocess import Popen, PIPE
 from setuptools import setup
-from torch.utils.cpp_extension import CppExtension, CUDAExtension, BuildExtension
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
 from distutils.sysconfig import get_python_inc
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 def run_command(cmd):
@@ -26,7 +45,7 @@ run_command('make -j%d' % os.cpu_count())
 # Python interface
 setup(
     name='MinkowskiEngine',
-    version='0.2.2',
+    version=find_version('MinkowskiEngine', '__init__.py'),
     install_requires=['torch'],
     packages=[
         'MinkowskiEngine', 'MinkowskiEngine.utils'
