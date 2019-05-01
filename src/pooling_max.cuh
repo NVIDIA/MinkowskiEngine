ac3173ca277f2f7f13300eaeaf24c9de063b4891
Chris Choy <chrischoy@ai.stanford.edu>
2019-05-01 07:44:55 -0700

    thrust max pool removed. sparse conv md added

--- src/pooling_max.cuh
+++ src/pooling_max.cuh
@@ -19,17 +19,4 @@ void MaxPoolingBackwardKernelGPU(Dtype *d_grad_in_feat, int in_nrows,
                                  const Dtype *d_grad_out_feat, int out_nrows,
                                  const Itype *d_max_index, int nchannel,
                                  cudaStream_t stream);
-
-template <typename Dtype, typename Itype>
-void ThrustMaxPoolingForwardKernelGPU(
-    const Dtype *d_in_feat, Dtype *d_out_feat, int out_nrows,
-    Itype *d_max_index, int nchannel,
-    const std::vector<std::vector<Itype>> &in_map,
-    const std::vector<std::vector<Itype>> &out_map, cudaStream_t stream);
-
-template <typename Dtype, typename Itype>
-void ThrustMaxPoolingBackwardKernelGPU(Dtype *d_grad_in_feat, int in_nrows,
-                                       const Dtype *d_grad_out_feat,
-                                       int out_nrows, const Itype *d_max_index,
-                                       int nchannel, cudaStream_t stream);
 #endif
