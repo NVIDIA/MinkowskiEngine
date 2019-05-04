08f886037fa5cebca502608f29d31146cc5a5533
Chris Choy <chrischoy@ai.stanford.edu>
2019-05-03 21:16:56 -0700

    use atlas for CBLAS

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
             sources=[
                 'pybind/minkowski.cpp',
             ],
-            libraries=['minkowski', 'openblas'],
+            libraries=['minkowski', 'atlas'],
             library_dirs=['objs'],
             # extra_compile_args=['-g']
         )
