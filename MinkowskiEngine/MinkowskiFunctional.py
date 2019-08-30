00c46d4b837031fbf0603ec16a686f3c7ef5c2e5
Chris Choy <chrischoy@ai.stanford.edu>
2019-08-30 05:28:03 -0700

    functionals

--- MinkowskiEngine/MinkowskiFunctional.py
+++ MinkowskiEngine/MinkowskiFunctional.py
@@ -26,13 +26,95 @@ import torch.nn.functional as F
 from SparseTensor import SparseTensor
 
 
-def relu(input):
-    output = F.relu(input.F)
-    return SparseTensor(
-        output, coords_key=input.coords_key, coords_manager=input.coords_man)
+# Activations
+def relu(input, *args, **kwargs):
+  output = F.relu(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
 
 
-def sigmoid(input):
-    output = F.sigmoid(input.F)
-    return SparseTensor(
-        output, coords_key=input.coords_key, coords_manager=input.coords_man)
+def prelu(input, *args, **kwargs):
+  output = F.prelu(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def leaky_relu(input, *args, **kwargs):
+  output = F.leaky_relu(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def elu(input, *args, **kwargs):
+  output = F.elu(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def celu(input, *args, **kwargs):
+  output = F.celu(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def softmax(input, *args, **kwargs):
+  output = F.softmax(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def log_softmax(input, *args, **kwargs):
+  output = F.log_softmax(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def sigmoid(input, *args, **kwargs):
+  output = F.sigmoid(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def tanh(input, *args, **kwargs):
+  output = F.tanh(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+# Dropouts
+def dropout(input, *args, **kwargs):
+  output = F.dropout(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+# Normalization
+def normalize(input, *args, **kwargs):
+  output = F.normalize(input.F, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+# Loss functions
+def binary_cross_entropy(input, target, *args, **kwargs):
+  output = F.binary_cross_entropy(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def binary_cross_entropy_with_logits(input, target, *args, **kwargs):
+  output = F.binary_cross_entropy_with_logits(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def cross_entropy(input, target, *args, **kwargs):
+  output = F.cross_entropy(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def kl_div(input, target, *args, **kwargs):
+  output = F.kl_div(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def l1_loss(input, target, *args, **kwargs):
+  output = F.l1_loss(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def mse_loss(input, target, *args, **kwargs):
+  output = F.mse_loss(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+
+
+def nll_loss(input, target, *args, **kwargs):
+  output = F.nll_loss(input.F, target, *args, **kwargs)
+  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
