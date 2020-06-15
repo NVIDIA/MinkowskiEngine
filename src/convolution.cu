2ba4f56b645e8915f887642dbc74d7b936da1b97
xmyqsh <xmyqsh@gmail.com>
2020-06-15 21:28:02 +0800

    demo of ILP(instruction level of parallel) on matmul in convolution.cu

--- src/convolution.cu
+++ src/convolution.cu
@@ -54,52 +54,54 @@ __global__ void matmul(const Dtype *A, const int wA, const int hA,
   const int x = BLOCK_SIZE * bx + tx;
   const int y = BLOCK_SIZE * by + ty;
 
+  const int NumILP = (wA + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
   // Csub is used to store the element of the block sub-matrix
   // that is computed by the thread
   Dtype Csub = 0;
+
+  // Declaration of the shared memory array As used to
+  // store the sub-matrix of A
+  __shared__ Dtype As[NumILP][BLOCK_SIZE][BLOCK_SIZE];
+
+  // Declaration of the shared memory array Bs used to
+  // store the sub-matrix of B
+  __shared__ Dtype Bs[NumILP][BLOCK_SIZE][BLOCK_SIZE];
 
   const Itype in_row = y < hA ? in_map[y] : 0;
   const Itype out_row = y < hA ? out_map[y] : 0;
 
   // Loop over all the sub-matrices of A and B
   // required to compute the block sub-matrix
+#pragma unroll
   for (int s = 0; s < wA; s += BLOCK_SIZE) {
-    // Declaration of the shared memory array As used to
-    // store the sub-matrix of A
-    __shared__ Dtype As[BLOCK_SIZE][BLOCK_SIZE];
-
-    // Declaration of the shared memory array Bs used to
-    // store the sub-matrix of B
-    __shared__ Dtype Bs[BLOCK_SIZE][BLOCK_SIZE];
-
     // Load the matrices from device memory
     // to shared memory; each thread loads
     // one element of each matrix
-    As[ty][tx] = ((s + tx) < wA && y < hA) ? A[wA * in_row + s + tx] : 0;
-    Bs[ty][tx] = ((s + ty) < hB && x < wB) ? B[wB * (s + ty) + x] : 0;
+    As[s][ty][tx] = ((s + tx) < wA && y < hA) ? A[wA * in_row + s + tx] : 0;
+    Bs[s][ty][tx] = ((s + ty) < hB && x < wB) ? B[wB * (s + ty) + x] : 0;
+  }
 
-    // Synchronize to make sure the matrices are loaded
-    __syncthreads();
+  // Synchronize to make sure the matrices are loaded
+  __syncthreads();
 
-    // Multiply the two matrices together;
-    // each thread computes one element
-    // of the block sub-matrix
+  // Multiply the two matrices together;
+  // each thread computes one element
+  // of the block sub-matrix
+#pragma unroll
+  for (int s = 0; s < wA; s += BLOCK_SIZE) {
 #pragma unroll
     for (int k = 0; k < BLOCK_SIZE; ++k) {
-      Csub += As[ty][k] * Bs[k][tx];
+      Csub += As[s][ty][k] * Bs[s][k][tx];
     }
-
-    // Synchronize to make sure that the preceding
-    // computation is done before loading two new
-    // sub-matrices of A and B in the next iteration
-    __syncthreads();
   }
 
   // Write the block sub-matrix to device memory;
   // each thread writes one element
   if (y < hA && x < wB)
-    atomicAdd(&C[wB * out_row + x], Csub);
+  //  atomicAdd(&C[wB * out_row + x], Csub);
   // C[wB * out_row + x] += Csub;
+    C[wB * out_row + x] = Csub;
 }
 
 /**
