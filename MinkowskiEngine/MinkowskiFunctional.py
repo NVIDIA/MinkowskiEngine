19b62f408afbdc7fd338dfa262b211463c35c473
Chris Choy <cchoy@nvidia.com>
2020-08-01 17:11:20 -0700

    update functional

--- MinkowskiEngine/MinkowskiFunctional.py
+++ MinkowskiEngine/MinkowskiFunctional.py
@@ -28,93 +28,165 @@ from MinkowskiSparseTensor import SparseTensor
 
 # Activations
 def relu(input, *args, **kwargs):
-  output = F.relu(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.relu(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def prelu(input, *args, **kwargs):
-  output = F.prelu(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.prelu(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def leaky_relu(input, *args, **kwargs):
-  output = F.leaky_relu(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.leaky_relu(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def elu(input, *args, **kwargs):
-  output = F.elu(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.elu(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def celu(input, *args, **kwargs):
-  output = F.celu(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.celu(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def softmax(input, *args, **kwargs):
-  output = F.softmax(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.softmax(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def log_softmax(input, *args, **kwargs):
-  output = F.log_softmax(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.log_softmax(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def sigmoid(input, *args, **kwargs):
-  output = F.sigmoid(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.sigmoid(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def tanh(input, *args, **kwargs):
-  output = F.tanh(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.tanh(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 # Dropouts
 def dropout(input, *args, **kwargs):
-  output = F.dropout(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.dropout(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 # Normalization
 def normalize(input, *args, **kwargs):
-  output = F.normalize(input.F, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.normalize(input.F, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 # Loss functions
 def binary_cross_entropy(input, target, *args, **kwargs):
-  output = F.binary_cross_entropy(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.binary_cross_entropy(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def binary_cross_entropy_with_logits(input, target, *args, **kwargs):
-  output = F.binary_cross_entropy_with_logits(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.binary_cross_entropy_with_logits(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def cross_entropy(input, target, *args, **kwargs):
-  output = F.cross_entropy(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.cross_entropy(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def kl_div(input, target, *args, **kwargs):
-  output = F.kl_div(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.kl_div(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def l1_loss(input, target, *args, **kwargs):
-  output = F.l1_loss(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.l1_loss(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def mse_loss(input, target, *args, **kwargs):
-  output = F.mse_loss(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.mse_loss(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
 
 
 def nll_loss(input, target, *args, **kwargs):
-  output = F.nll_loss(input.F, target, *args, **kwargs)
-  return SparseTensor(output, coords_key=input.coords_key, coords_manager=input.coords_man)
+    output = F.nll_loss(input.F, target, *args, **kwargs)
+    return SparseTensor(
+        output,
+        coordinate_map_key=input.coordinate_map_key,
+        coordinate_manager=input.coordinate_manager,
+    )
